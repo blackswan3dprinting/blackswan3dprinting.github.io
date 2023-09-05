81129754c4f828c589e6a7ef35364d6535ea2b1d
Carlos Valdez <carlos@blackswan3d.com>
2023-09-05 15:45:25 -0700

    minor additions to our_team.html

--- quick_reload.py
+++ quick_reload.py
@@ -7,7 +7,7 @@
 import os
 
 # include the .scss files you're working on here
-SASS_FILES = ['index']
+SASS_FILES = ['our_team']
 assert len(SASS_FILES), "quick_reload.py: SASS_FILES cannot be empty."
 
 
