4f6aca446e3e7ff2870c79fbb291aeae6745209f
Carlos Valdez <cvaldezh@uci.edu>
2023-09-08 04:02:18 -0700

    feat: index changed

--- src/bs3d/website/views.py
+++ src/bs3d/website/views.py
@@ -3,15 +3,18 @@
 #
 # https://blackswan3d.com/
 
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect
 
 bp = Blueprint('website', __name__,
                template_folder='templates',
                static_folder='static')
 
+@bp.route("/")
+def index():
+    return redirect('/about/')
 
 @bp.route('/showcase/')
-def index():
+def showcase():
     return render_template('showcase.html')
 
 @bp.route("/about/")
