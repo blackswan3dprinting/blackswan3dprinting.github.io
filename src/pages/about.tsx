4cc1cc483133ba1c07c324f8d9bad675aec40846
Carlos Valdez <cvaldezh@uci.edu>
2023-09-15 07:54:50 -0700

    feat: created under header text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,10 @@
 import Layout from "../components/layout"
+import { UnderHeaderText } from "../components/under_header_text"
 
 export default function About() {
     return (
         <Layout>
-            
+            <UnderHeaderText text="ABOUT US"/>
         </Layout>
     )
 }
