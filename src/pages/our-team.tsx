cd3e79d48020fd850a1938b1fb0d6899fb1fb95b
Carlos Valdez <cvaldezh@uci.edu>
2023-09-15 07:56:16 -0700

    feat: created /our-team/ and /our-work/

---
+++ src/pages/our-team.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout"
+import { UnderHeaderText } from "../components/under_header_text"
+
+export default function OurTeam() {
+    return (
+        <Layout>
+            <UnderHeaderText text="OUR TEAM"/>
+        </Layout>
+    )
+}
---
+++ src/pages/our-work.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout"
+import { UnderHeaderText } from "../components/under_header_text"
+
+export default function OurWork() {
+    return (
+        <Layout>
+            <UnderHeaderText text="OUR WORK"/>
+        </Layout>
+    )
+}
