3420eeda83ad115b25983fff1010b73902d5105f
Carlos Valdez <carlos@blackswan3d.com>
2023-09-15 10:49:41 -0700

    feat: etsy shop somewhat integrated

---
+++ src/components/etsy_listing.tsx
@@ -0,0 +1,14 @@
+import styles from "../styles/etsy_listing.module.scss"
+
+export function EtsyListing(props: {name: string, desc: string, url: string}) {
+    return (
+        <div className={styles.etsy_listing}>
+            <h2>{props.name}</h2>
+            <p>{props.desc}</p>
+
+            <a href={props.url} className={styles.etsy_button} target="_blank" rel="noopener noreferrer">
+                View on <strong>Etsy</strong>
+            </a>
+        </div>
+    )
+}
