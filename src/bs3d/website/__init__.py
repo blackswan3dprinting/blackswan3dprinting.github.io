8a0f7692a6ecc1dc61bfc4f1b6223defaf9deb75
Carlos Valdez <cvaldezh@uci.edu>
2023-09-15 03:49:51 -0700

    refactor: removed all Python

--- src/bs3d/website/__init__.py
+++
@@ -1,6 +0,0 @@
-# bs3d/website/__init__.py
-# Black Swan 3D Printing
-#
-# Initiates the website package.
-
-from . import views
--- wsgi.py
+++
@@ -1,9 +0,0 @@
-# wsgi.py
-# Black Swan 3D Printing
-#
-# Initiates the website.
-
-from src.app import app
-
-if __name__ == '__main__':
-    app.run()
