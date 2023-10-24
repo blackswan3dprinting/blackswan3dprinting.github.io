9e05bfbf0501aab1696ede88b231f123b464a0ab
Carlos Valdez <calejvaldez@gmail.com>
2023-10-23 23:56:40 -0700

    feat: finished 1/4 part of index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import styles from '../styles/index.module.scss';
+import { Raleway } from 'next/font/google';
+
+const raleway = Raleway({
+    subsets: ['latin']
+})
+
 export default function Index() {
     return (
-        <>
+        <div className={styles.body}>
+            <div className={`${styles.headline} ${raleway.className}`}>
+                <h1>YOUR PREMIER 3D PRINTING SERVICE.</h1>
+                <p>Get your ideas printed with the highest quality materials from a business you can trust.</p>
+                <a href="">View our Services ➤</a>
+            </div>
             
-        </>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     )
 }
