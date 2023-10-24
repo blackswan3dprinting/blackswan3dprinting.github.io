166223cc50f213cd9245b980d1cbc899311e0ce5
Carlos Valdez <calejvaldez@gmail.com>
2023-10-24 10:04:51 -0700

    feat: fonts properly synced

--- src/app/etsy_listing.tsx
+++ src/app/etsy_listing.tsx
@@ -1,10 +1,15 @@
 import styles from "../styles/etsy_listing.module.scss"
 import { Button } from "./button"
+import { Raleway } from "next/font/google"
+
+const raleway = Raleway({
+    subsets: ['latin']
+})
 
 export function EtsyListing(props: {name: string, desc: string, url: string}) {
     return (
         <div className={styles.etsy_listing}>
-            <h1>{props.name}</h1>
+            <h1 className={raleway.className} >{props.name}</h1>
             <p>{props.desc}</p>
 
             <Button label="ORDER ON ETSY  ➤" url={props.url} />
