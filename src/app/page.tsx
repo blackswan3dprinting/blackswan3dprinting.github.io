0ff7221408bb78a475788b20d6d1a234f98bc6db
Carlos Valdez <calejvaldez@gmail.com>
2023-10-24 00:14:15 -0700

    feat: 2/4 of the design complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,17 @@ export default function Index() {
                 <p>Get your ideas printed with the highest quality materials from a business you can trust.</p>
                 <a href="">View our Services ➤</a>
             </div>
+
+            <div id={styles.what_we_do}>
+                <div id={styles.what_we_do_text}>
+                    <h1 className={raleway.className}>WHAT WE DO</h1>
+                    <p>At Black Swan 3D Printing, our priority is to give you the highest quality 3D prints at the best rates in the industry. We strive to be your most reliable 3D print-on-demand service. Whether you need an idea modeled with a Computer Aided Design tool or have a part that needs to be printed, we will make sure to assist with all your printing needs. </p>
+                </div>
+                <img src="/images/deadpool.jpg" height="250" alt="3D print of the Marvel character Deadpool"></img>
+            </div>
+
+            <div></div>
             
-            <div></div>
-            <div></div>
             <div></div>
         </div>
     )
