58c4760566e964c7d8c7b14a662c7f0b28765ef3
Carlos Valdez <calejvaldez@gmail.com>
2023-10-23 22:21:37 -0700

    style: header updated

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -15,9 +15,10 @@ export function Header() {
         <header className={styles.header}>
             <Image src="/images/transparent-logo-bird.svg" alt="Black Swan logo" width="100" height="100"/>
             <div id={styles.header_menu_buttons}>
+                <HeaderButton label="Home" link="/"/>
                 <HeaderButton label="About" link="/about/"/>
-                <HeaderButton label="Our Work" link="/our-work/"/>
-                <HeaderButton label="Our Team" link="/our-team/"/>
+                <HeaderButton label="Services" link="/our-work/"/>
+                <HeaderButton label="Contact" link="/our-team/"/>
             </div>
         </header>
     )
